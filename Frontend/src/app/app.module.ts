7313615afbd21a293a9c6219d57653cfa120f30d
Shoaib Karamat <shoaib.karamat@jjtestsite.us>
2020-11-06 13:18:12 +0500

    Create a seprate json file and load data using the service httpclient in our constructor

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HousingService } from './services/housing.service';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
@@ -16,9 +18,12 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
    ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HousingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-property-list',
@@ -7,72 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  Properties: any = [
-    {
-      "Id": 1,
-      "Name": "Joe House",
-      "Type": "House",
-      "Price": 5000000
-    },
-    {
-      "Id": 1,
-      "Name": "Trump House",
-      "Type": "House",
-      "Price": 5000000
-    },
-    {
-      "Id": 2,
-      "Name": "Senate House",
-      "Type": "House",
-      "Price": 5000000
-    },
-    {
-      "Id": 3,
-      "Name": "Parliment House",
-      "Type": "House",
-      "Price": 5000000
-    },
-    {
-      "Id": 4,
-      "Name": "Steven House",
-      "Type": "House",
-      "Price": 5000000
-    },
-    {
-      "Id": 5,
-      "Name": "Obama House",
-      "Type": "House",
-      "Price": 5000000
-    },
-    {
-      "Id": 6,
-      "Name": "Kevin House",
-      "Type": "House",
-      "Price": 5000000
-    },
-    {
-      "Id": 7,
-      "Name": "Jhon House",
-      "Type": "House",
-      "Price": 5000000
-    },
-    {
-      "Id": 8,
-      "Name": "Snow House",
-      "Type": "House",
-      "Price": 5000000
-    },
-    {
-      "Id": 9,
-      "Name": "Marken House",
-      "Type": "House",
-      "Price": 5000000
-    }
-  ]
+  //Properties: Array<any>;
+  Properties: any;
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+
+  }
 
   ngOnInit(): void {
+    this.http.get('data/properties.json').subscribe(
+      data=> {
+        this.Properties = data;
+        console.log(data);
+
+      }
+    )
   }
 
 }
